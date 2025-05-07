7fb01f7a1c2feb1d6ce9435f38a0ddd623267641
Mushfique Alam <162487127+MushfiqueAlam@users.noreply.github.com>
2025-05-07 16:29:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+  const table=document.querySelector("#sampleTable")
+    const row = table.insertRow(0); // Insert a new row at the end of the table
+    const cell1 = row.insertCell(0); // Insert a new cell in the row
+    const cell2 = row.insertCell(1); // Insert a new cell in the row
+    cell1.innerHTML = "New Cell1"; // Add some text to the new cell
+    cell2.innerHTML = "New Cell2"; // Add some text to the new cell
+
   
 }
